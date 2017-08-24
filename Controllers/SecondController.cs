e38fb51468f6fcc58869f39ec44bdd5aa5061c2c
VitalieBraga <vitaliebraga@gmail.com>
2017-08-23 18:08:51 -0700

    API estableshed with the Python app.

--- Controllers/SecondController.cs
+++ Controllers/SecondController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Auction.Models;
 using System.Linq;
+using Newtonsoft.Json;
+
 
 namespace Auction.Controllers
 {
@@ -23,33 +25,39 @@ namespace Auction.Controllers
         [Route("dashboard")]
         public IActionResult Index()
         {
-            var Products = new Dictionary<string, object>();
+            // if (HttpContext.Session.GetInt32("UserId") == null)
+            // {
+            //     TempData["NiceTry"] = "You need to be logged in to view account info!";
+            //     return RedirectToAction("LoginPage", "Register");
+            // }
+            var Products = new List<Dictionary<string, object>>();
             WebRequest.GetProductDataAsync(ApiResponse =>
                 {
                     Products = ApiResponse;
                 }
             ).Wait();
-            System.Console.WriteLine(Products);
+            ViewBag.Products = Products;
             return View("Dashboard");
         }
 
 
-        // [HttpGet]
-        // [Route("pokemon/{pokeid}")]
-        // public IActionResult QueryPoke(int pokeid)
-        // {
-        //     var PokeInfo = new Dictionary<string, object>();
-        //     WebRequest.GetPokemonDataAsync(pokeid, ApiResponse =>
-        //         {
-        //             PokeInfo = ApiResponse;
-        //         }
-        //     ).Wait();
-        //     // Other code
-        //     ViewBag.name = PokeInfo["name"];
-        //     ViewBag.height = PokeInfo["height"];
-        //     ViewBag.weight = PokeInfo["weight"];
-
-        //     return View("Pokemon");
-        // }
+        [HttpGet]
+        [Route("getProduct/{id}")]
+        public IActionResult Index(int id)
+        {
+            // if (HttpContext.Session.GetInt32("UserId") == null)
+            // {
+            //     TempData["NiceTry"] = "You need to be logged in to view account info!";
+            //     return RedirectToAction("LoginPage", "Register");
+            // }
+            var ProductInfo = new Dictionary<string, object>();
+            WebRequest.SingleProductAsync(id, ApiResponse =>
+                {
+                    ProductInfo = ApiResponse;
+                }
+            ).Wait();
+            ViewBag.Product = ProductInfo;
+            return View("OneProduct");
+        }
     }
 }
